5241c86ed058eb5b4a8b9474d2f6b9b44ecf9f73
nethushinavodya <nethushiperera@gmail.com>
2025-03-23 09:43:27 +0530

    Commit

--- src/main/java/dto/TherapistDTO.java
+++ src/main/java/dto/TherapistDTO.java
@@ -1,4 +1,18 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
 public class TherapistDTO {
+    private String TherapistId;
+    private String name;
+    private String specialization;
+    private String contactNo;
 }
